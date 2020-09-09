bc802e1b90c5c674043d76b798be03e9bab541cf
Raj Bhatta <bhattaraj21@gmail.com>
2020-09-09 15:49:18 -0700

    Chain of responsibility update

---
+++ 01-Chain-Of-Responsibility-Pattern/src/main/java/example/one/ErrorLogger.java
@@ -0,0 +1,13 @@
+package example.one;
+
+public class ErrorLogger extends AbstractLogger {
+
+    public ErrorLogger(int level){
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("Error Console::Logger: " + message);
+    }
+}
---
+++ 01-Chain-Of-Responsibility-Pattern/src/main/java/example/one/FileLogger.java
@@ -0,0 +1,13 @@
+package example.one;
+
+public class FileLogger extends AbstractLogger {
+
+    public FileLogger(int level){
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("File::Logger: " + message);
+    }
+}
