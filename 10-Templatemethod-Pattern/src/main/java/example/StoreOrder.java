d84a798d0c60ef38cf50e24c2792fe5f393b0720
Raj Bhatta <bhattaraj21@gmail.com>
2020-10-03 23:44:44 -0700

    Template Design Pattern

---
+++ 10-Templatemethod-Pattern/src/main/java/example/StoreOrder.java
@@ -0,0 +1,24 @@
+package example;
+
+public class StoreOrder extends OrderProcessTemplate
+{
+
+    @Override
+    public void doSelect()
+    {
+        System.out.println("Customer chooses the item from shelf.");
+    }
+
+    @Override
+    public void doPayment()
+    {
+        System.out.println("Pays at counter through cash/POS");
+    }
+
+    @Override
+    public void doDelivery()
+    {
+        System.out.println("Item delivered to in delivery counter.");
+    }
+
+}
